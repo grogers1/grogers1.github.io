2a480f00bec14ff98956e0e186f1d79e5610dad1
grogers1 <60073528+grogers1@users.noreply.github.com>
2020-10-13 16:51:33 -0400

    Add files via upload

---
+++ public/lab_5/main.js
@@ -0,0 +1,24 @@
+<script type="text/javascript">
+    {
+    btn.onclick = function();
+    const btn = document.querySelector('activate');
+    { 
+    function testFunction() {
+    fetch('/api')
+    .then((response) => response.text())
+    .then((response) => {
+         console.log(response);
+    const myHeading = document.querySelector('h1');
+    myHeading.textContent = response;
+    function setColor(element, color)
+    {
+    element.style.backgroundColor = color;
+    }
+    var body = document.getElementById('body');
+    setColor(body, 'green');
+
+    
+
+    })
+}}
+</script>
